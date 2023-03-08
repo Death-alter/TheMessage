a2d62ac29b602f701b32d1627cbf0db8495b4202
Death-alter <42757138+Death-alter@users.noreply.github.com>
2023-03-08 23:58:36 +0800

    Delete assets/Scripts directory

--- assets/Scripts/UI/Resident/Background.ts
+++
@@ -1,9 +0,0 @@
-import { _decorator, Component, director } from "cc";
-const { ccclass, property } = _decorator;
-
-@ccclass("Background")
-export class Background extends Component {
-  start() {
-    director.addPersistRootNode(this.node);
-  }
-}
--- assets/Scripts/config.ts
+++
@@ -1,7 +0,0 @@
-const config = {
-  webSocketUrl: "ws://localhost:12222/ws",
-  heartBeatInterval: 20,
-  maxRetryTimes: 3,
-};
-
-export default config;
