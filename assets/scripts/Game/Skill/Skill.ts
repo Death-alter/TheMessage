4e16794275d281a7704c55d186a19bd0a78a15a4
Death <546115696@qq.com>
2023-06-05 17:30:17 +0800

    init

--- assets/scripts/Game/Skill/Skill.ts
+++ assets/scripts/Game/Skill/Skill.ts
@@ -1,10 +1,9 @@
 import { Character } from "../Character/Character";
-import { SkillType, SkillOption, ActiveSkillOption, PassiveSkillOption, SkillCondition } from "./type";
+import { SkillOption } from "./type";
 
 export abstract class Skill {
   protected _name: string;
   protected _character: Character;
-  protected _type: SkillType;
   protected _description: string;
 
   get name() {
@@ -15,10 +14,6 @@ export abstract class Skill {
     return this._character;
   }
 
-  get type() {
-    return this._type;
-  }
-
   get description() {
     return this._description;
   }
@@ -26,38 +21,10 @@ export abstract class Skill {
   constructor(option: SkillOption) {
     this._name = option.name;
     this._character = option.character;
-    this._type = option.type;
     this._description = option.description;
   }
-}
 
-export abstract class ActiveSkill extends Skill {
-  protected _condition: SkillCondition[];
+  abstract init(): void;
 
-  get condition() {
-    return this._condition;
-  }
-
-  constructor(option: ActiveSkillOption) {
-    super({
-      name: option.name,
-      character: option.character,
-      type: SkillType.ACTIVE,
-      description: option.description,
-    });
-    this._condition = option.condition;
-  }
-
-  abstract onUse(): void;
-}
-
-export abstract class PassiveSkill extends Skill {
-  constructor(option: PassiveSkillOption) {
-    super({
-      name: option.name,
-      character: option.character,
-      type: SkillType.PASSIVE,
-      description: option.description,
-    });
-  }
+  abstract dispose(): void;
 }
--- assets/scripts/Game/Skill/SkillClass/JianRen.ts
+++ assets/scripts/Game/Skill/SkillClass/JianRen.ts
@@ -1,29 +1,32 @@
-import { GameEvent } from "../../../Event/type";
-import { ActiveSkill } from "../Skill";
-import { Player } from "../../Player/Player";
+import { skill_jian_ren_a_toc, skill_jian_ren_b_toc } from "../../../../protobuf/proto";
+import { NetworkEventCenter } from "../../../Event/EventTarget";
+import { NetworkEventToC, NetworkEventToS } from "../../../Event/type";
 import { Character } from "../../Character/Character";
-import { PlayerReceiveMessage } from "../../../Event/GameEventType";
-import { CardColor } from "../../Card/type";
+import { Skill } from "../Skill";
 
-export class JianRen extends ActiveSkill {
+export class JianRen extends Skill {
   constructor(character: Character) {
     super({
       name: "坚韧",
       character: character,
       description:
         "你接收黑色情报后，可以展示牌堆顶的一张牌，若是黑色牌，则将展示的牌加入你的手牌，并从一名角色的情报区弃置一张黑色情报。",
-      condition: [
-        {
-          event: GameEvent.PLAYER_RECEIVE_MESSAGE,
-          enabled({ player, message }: PlayerReceiveMessage) {
-            if (player.character === this.character && message.color.indexOf(CardColor.BLACK) !== -1) {
-              return true;
-            }
-          },
-        },
-      ],
     });
   }
+
+  init() {
+    NetworkEventCenter.on(NetworkEventToC.SKILL_JIAN_REN_A_TOC, this.onUseJianRenA, this);
+    NetworkEventCenter.on(NetworkEventToC.SKILL_JIAN_REN_B_TOC, this.onUseJianRenB, this);
+  }
+
+  dispose() {
+    NetworkEventCenter.off(NetworkEventToC.SKILL_JIAN_REN_A_TOC, this.onUseJianRenA);
+    NetworkEventCenter.off(NetworkEventToC.SKILL_JIAN_REN_B_TOC, this.onUseJianRenB);
+  }
+
+  onUseJianRenA(data: skill_jian_ren_a_toc) {}
+
+  onUseJianRenB(data: skill_jian_ren_b_toc) {}
 
   onUse() {}
 }
